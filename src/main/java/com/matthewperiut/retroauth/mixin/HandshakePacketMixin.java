ed368683b8620b56063bec85a877f561c4ed2cec
Matthew Periut <matthewperiut@gmail.com>
2025-08-10 14:59:19 -0400

    Fix Packet Limit Crash

--- src/main/java/com/matthewperiut/retroauth/mixin/HandshakePacketMixin.java
+++ src/main/java/com/matthewperiut/retroauth/mixin/HandshakePacketMixin.java
@@ -4,14 +4,21 @@ import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.handshake.HandshakePacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.io.DataInputStream;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(HandshakePacket.class)
-abstract public class HandshakePacketMixin extends Packet {
-    @Redirect(method = "read", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/handshake/HandshakePacket;readString(Ljava/io/DataInputStream;I)Ljava/lang/String;"))
-    String redir(DataInputStream dataInputStream, int i) {
-        return readString(dataInputStream, 40);
+abstract class HandshakePacketMixin extends Packet {
+
+    @ModifyArg(
+            method = "read",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/network/packet/handshake/HandshakePacket;readString(Ljava/io/DataInputStream;I)Ljava/lang/String;"
+            ),
+            index = 1
+    )
+    private int widenMaxLen(int originalMax) {
+        return Math.max(originalMax+8, 40);
     }
 }
+
